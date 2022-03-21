5e13b4a8e4ac6cd66c9a426ac68f99912c214dd2
gaikwadmohit <97322025+gaikwadmohit@users.noreply.github.com>
2022-03-21 16:08:22 +0530

    Add files via upload

---
+++ Day 2/If -Else Statement Problems/ReadAndTellPlaceValue.java
@@ -0,0 +1,32 @@
+package Day2Assignments;
+
+import java.util.Scanner;
+
+public class ReadAndTellPlaceValue {
+
+	public static void main(String[] args) {
+		
+        int hundreds = 0;
+        int tens = 0;
+        int ones = 0;
+
+           
+        System.out.println("below shows the place value of 1,10,100,1000");
+        
+
+        //Displays hundreds place digit
+        
+        System.out.println("Hundreds place digit: "+ 0 );
+
+        //Displays tens digit
+        
+        System.out.println("Tens place digit: " + 0);
+
+
+        //Display ones digit
+       
+        System.out.println("Ones place digit: " + 0 );   
+
+	}
+
+}
