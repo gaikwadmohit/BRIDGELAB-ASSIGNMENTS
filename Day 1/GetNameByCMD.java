40a7c6e118d3e7ab57dae034b20a9afbbf8cd453
gaikwadmohit <97322025+gaikwadmohit@users.noreply.github.com>
2022-03-21 14:19:17 +0530

    Add files via upload

---
+++ Day 1/GetNameByCMD.java
@@ -0,0 +1,11 @@
+package FIRSTPROJECT;
+
+public class GetNameByCMD {
+
+	public static void main(String args[]) {
+		
+			System.out.println("Your first argument is: "+args[1]);  
+			
+			}  
+			
+}
