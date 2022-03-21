0867e011a4668eeb2a3299094df6cc11b19a33de
gaikwadmohit <97322025+gaikwadmohit@users.noreply.github.com>
2022-03-21 16:15:10 +0530

    Program for the sum of n natural numbers eg. Input N=5 output 1+2+3+4+5

---
+++ Day 2/Programs With While loop/SumOfNaturalNumber.java
@@ -0,0 +1,25 @@
+package Day2Assignments;
+
+import java.util.Scanner;
+
+public class SumOfNaturalNumber {
+
+	public static void main(String[] args) {
+		 int n = 10, sum = 0, i = 1;
+		  
+	        
+	        while (i <= n) {
+	  
+	           
+	            sum = sum + i;
+	  
+	            i++;
+	        }
+	  
+	        
+	        System.out.println(
+	            "Sum of natural numbers using while loop is:"
+	            + " " + sum);
+	    }
+	}
+
