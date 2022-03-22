6949bd44cb58df73f94aef2e29daf5113e379ede
Mohit Gaikwad <97322025+gaikwadmohit@users.noreply.github.com>
2022-03-22 12:23:29 +0530

    Update VowelsOrConsonent.java

--- Day 2/Switch Statement/VowelsOrConsonent.java
+++ Day 2/Switch Statement/VowelsOrConsonent.java
@@ -1,1 +1,30 @@
+package Day2Assignments;
+
+import java.util.Scanner;
+
+public class VowelsOrConsonent {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		 char ch ;
+		 Scanner s=new Scanner(System.in);
+		 System.out.println("Enetr any character");
+		 ch=s.next().charAt(0);
+		 
+
+	        switch (ch) {
+	            case 'a':
+	            case 'e':
+	            case 'i':
+	            case 'o':
+	            case 'u':
+	                System.out.println(ch + " is vowel");
+	                break;
+	            default:
+	                System.out.println(ch + " is consonant");
+	        }
+	    }
+	}
+
 
